e405a1ac7c0f85138de29a705d9d316203e8ed8f
James Brechtel <jbrechtel@gmail.com>
2013-07-08 18:32:49 -0400

    basic working proxy

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject middler-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An HTTP debugging proxy"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"] [http-kit "2.1.2"]]
   :main middler-clojure.core)
--- src/middler_clojure/core.clj
+++ src/middler_clojure/core.clj
@@ -1,9 +1,27 @@
 (ns middler-clojure.core
+  (:use org.httpkit.server)
+  (:use clojure.walk)
+  (:require [org.httpkit.client :as http])
   (:gen-class))
 
+(defn sanitize-headers [m]
+  (-> (stringify-keys m)
+      (dissoc "content-length")
+      (dissoc "content-encoding")
+      (dissoc "host")))
+
+(defn app [{:keys [request-method headers body] :as req}]
+  (println (str "Pre-sanitization"))
+  (println headers)
+  (println (str "Post-sanitization"))
+  (println (sanitize-headers headers))
+  (let [uri (:uri req)]
+    (with-channel req channel
+      (http/request {:url uri :method request-method :headers (sanitize-headers headers) :body body}
+                    (fn [res]
+                      (let [response-headers (sanitize-headers (:headers res))]
+                        (send! channel (assoc res :headers response-headers))))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (run-server app {:port 8080})) ; Ring server
