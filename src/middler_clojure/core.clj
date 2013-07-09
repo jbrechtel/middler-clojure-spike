1aa8207dc95534b9141706612265ceb95c0bc02c
James Brechtel <jbrechtel@gmail.com>
2013-07-08 20:39:39 -0400

    Support query strings, thanks for making this difficult http-kit

--- src/middler_clojure/core.clj
+++ src/middler_clojure/core.clj
@@ -10,17 +10,26 @@
       (dissoc "content-encoding")
       (dissoc "host")))
 
-(defn app [{:keys [request-method headers body] :as req}]
-  (println (str "Pre-sanitization"))
-  (println headers)
-  (println (str "Post-sanitization"))
-  (println (sanitize-headers headers))
-  (let [uri (:uri req)]
-    (with-channel req channel
-      (http/request {:url uri :method request-method :headers (sanitize-headers headers) :body body}
-                    (fn [res]
-                      (let [response-headers (sanitize-headers (:headers res))]
-                        (send! channel (assoc res :headers response-headers))))))))
+(defn assoc-query-string-part [m qs-part]
+  (let [k (first (clojure.string/split qs-part #"="))
+        v (second (clojure.string/split qs-part #"="))]
+    (assoc m k v)))
+
+
+(defn parse-query-string [qs]
+  (if (= nil qs)
+    {}
+    (let [decoded (java.net.URLDecoder/decode qs "UTF-8")]
+      (reduce assoc-query-string-part {} (clojure.string/split decoded #"&")))))
+
+(defn app [{:keys [uri request-method headers body query-string] :as req}]
+  (println (str "parsed querystring"))
+  (println (parse-query-string query-string))
+  (with-channel req channel
+    (http/request {:url uri :method request-method :headers (sanitize-headers headers) :body body :query-params (parse-query-string query-string)}
+                  (fn [res]
+                    (let [response-headers (sanitize-headers (:headers res))]
+                      (send! channel (assoc res :headers response-headers)))))))
 
 (defn -main
   [& args]
